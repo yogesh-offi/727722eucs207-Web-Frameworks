4f0f8629042800746badf7c7a84bf9eef1848c36
yogesh-offi <124981010+yogesh-offi@users.noreply.github.com>
2024-04-08 10:55:57 +0530

    Usecase

--- demo/src/main/java/com/project/demo/model/model.java
+++ demo/src/main/java/com/project/demo/model/model.java
@@ -1,13 +1,20 @@
 package com.project.demo.model;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.demo.response.Response;
+
 @RestController
 public class model {
-    @GetMapping("/Yogesh")
-    public String Controller()
+    @Value("${myname.name}")  //to define the name of an application
+    private String myname;
+
+    @GetMapping("/welcome")  //endpoint 
+    public Response Controller()
     {
-        return "Hello gud mornin";
+        Response r = new Response(1, "yogesh", "s");  //obj for response class
+        return r;
     }
 }
